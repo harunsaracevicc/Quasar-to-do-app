85da91885d09a529a3254d52e5ba5f130fd5f60c
Harun Saracevic <harunsaracevicc@gmail.com>
2022-10-15 02:46:53 +0200

    Changed colors and info that we dont have any tasks to do

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,114 +10,103 @@
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
-
-        
       </q-toolbar>
       <div class="q-px-lg q-pt-xl q-mb-md">
         <div class="text-h3">Todo app</div>
         <div class="text-subtitle1">{{ todaysDate }}</div>
       </div>
-      <q-img
-        src="../../images/mountains.jpg"
-        class="header-image absolute-top">
+      <q-img src="../../images/mountains.jpg" class="header-image absolute-top">
       </q-img>
     </q-header>
 
-     <q-drawer
-        v-model="leftDrawerOpen"
-        show-if-above
-        :width="250"
-        :breakpoint="600"
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :width="250"
+      :breakpoint="600"
+    >
+      <q-scroll-area
+        style="
+          height: calc(100% - 192px);
+          margin-top: 192px;
+          border-right: 1px solid #ddd;
+        "
       >
-        <q-scroll-area style="height: calc(100% - 192px); margin-top: 192px; border-right: 1px solid #ddd">
-          <q-list padding>
-            <q-item
-              to="/"
-              exact
-              clickable
-              v-ripple>
-              <q-item-section avatar>
-                <q-icon name="list" />
-              </q-item-section>
+        <q-list padding>
+          <q-item to="/" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="list" />
+            </q-item-section>
 
-              <q-item-section>
-                Todo
-              </q-item-section>
-            </q-item>
+            <q-item-section> Todo </q-item-section>
+          </q-item>
 
-            <q-item
-              to="/help"
-              exact
-              clickable
-              v-ripple>
-              <q-item-section avatar>
-                <q-icon name="help" />
-              </q-item-section>
+          <q-item to="/help" exact clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="help" />
+            </q-item-section>
 
-              <q-item-section>
-                Help
-              </q-item-section>
-            </q-item>
+            <q-item-section> Help </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
 
-           
-          </q-list>
-        </q-scroll-area>
-
-        <q-img class="absolute-top " src="../../images/mountains.jpg" style="height: 192px">
-          <div class="absolute-bottom bg-transparent">
-            <q-avatar size="56px" class="q-mb-sm">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-            </q-avatar>
-            <div class="text-weight-bold">Harun Saracevic</div>
-            <div>@harunsaracevicc</div>
-          </div>
-        </q-img>
-      </q-drawer>
+      <q-img
+        class="absolute-top left-header-img"
+        src="../../images/mountains.jpg"
+        style="height: 192px"
+      >
+        <div class="absolute-bottom bg-transparent">
+          <q-avatar size="56px" class="q-mb-sm">
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+          <div class="text-weight-bold">Harun Saracevic</div>
+          <div>@harunsaracevicc</div>
+        </div>
+      </q-img>
+    </q-drawer>
     <q-page-container>
-       <router-view />
+      <router-view />
     </q-page-container>
   </q-layout>
 </template>
 
 <script>
-import { date } from 'quasar'
-import { defineComponent, ref } from 'vue'
-
+import { date } from "quasar";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
-  name: 'MainLayout',
+  name: "MainLayout",
 
   components: {
     // EssentialLink
   },
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+  setup() {
+    const leftDrawerOpen = ref(false);
 
     return {
       leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
   },
 
   computed: {
-    todaysDate(){
-      let timeStamp = Date.now()
-      return date.formatDate(timeStamp, 'dddd D MMMM')
-    }
-  }
-  
-})
+    todaysDate() {
+      let timeStamp = Date.now();
+      return date.formatDate(timeStamp, "dddd D MMMM");
+    },
+  },
+});
 </script>
 
-
 <style lang="scss">
-  .header-image{
-    height:100%;
-    z-index: -1;
-    opacity: 0.5;
-    filter: grayscale(100%);
-  }
+.header-image {
+  height: 100%;
+  z-index: -1;
+  opacity: 0.7;
+  filter: grayscale(100%);
+}
 </style>
--- src/pages/TodoPage.vue
+++ src/pages/TodoPage.vue
@@ -1,153 +1,146 @@
 <template>
-  <q-page class="bg-grey-2 column" >
-    <div class="row q-pa-sm bg-primary">
-       <q-input
+  <q-page class="bg-grey-2 column">
+    <div class="row q-pa-sm bg-dark">
+      <q-input
         v-model="newTask"
         @keyup.enter="addTask"
         class="col"
         square
-        filled       
+        filled
         bg-color="white"
         placeholder="Add task"
-        dense>
-       
-
+        dense
+      >
         <template v-slot:append>
-          <q-btn
-            @click="addTask"
-            round
-            dense
-            flat
-            icon="add" />
+          <q-btn @click="addTask" round dense flat icon="add" />
         </template>
       </q-input>
     </div>
-   <q-list
-     separator
-     bordered
-     class="bg-white">
+    <q-list separator bordered class="bg-white">
       <q-item
         clickable
-        v-for='(task, index) in tasks'
+        v-for="(task, index) in tasks"
         :key="task.title"
         @click="task.done = !task.done"
-        :class="{ 'done bg-yellow-2' : task.done }"
-        v-ripple>
+        :class="{ 'done bg-yellow-2': task.done }"
+        v-ripple
+      >
         <q-item-section avatar>
           <q-checkbox
             class="no-pointer-events"
             v-model="task.done"
-            color="primary" />
-            </q-item-section>
+            color="primary"
+          />
+        </q-item-section>
         <q-item-section>
           <q-item-label>{{ task.title }}</q-item-label>
         </q-item-section>
-        <q-item-section
-        side
-        >
+        <q-item-section side>
           <q-btn
-           
-          @click.stop="editTask()"
-           dense
-           flat
-           round
-           color="primary"
-           icon="edit">
-
+            @click.stop="editTask()"
+            dense
+            flat
+            round
+            color="primary"
+            icon="edit"
+          >
           </q-btn>
         </q-item-section>
-        <q-item-section
-        class="align-end"
-         v-if="task.done"
-         side
-         >
-         <q-btn
-          @click.stop="deleteTask(index)"
-           dense
-           flat
-           round
-           color="primary"
-           icon="delete" />
+        <q-item-section class="align-end" v-if="task.done" side>
+          <q-btn
+            @click.stop="deleteTask(index)"
+            dense
+            flat
+            round
+            color="negative"
+            icon="delete"
+          />
         </q-item-section>
-        
       </q-item>
     </q-list>
+
+    <div
+      class="no-task absolute-center text-center no-task"
+      v-if="!tasks.length"
+    >
+      <q-icon name="check" size="100px"></q-icon>
+      <div class="text-h5">No tasks</div>
+    </div>
   </q-page>
 </template>
 
 <script>
-
-
-export default ({
+export default {
   data() {
     return {
-      newTask:"",
+      newTask: "",
       tasks: [
-        {
-          title: "Go to shopping",
-          done:false
-        },
-        {
-          title: "Go to gym",
-          done:false
-        },
-        {
-          title: "Study",
-          done:false
-        },
-        {
-          title: "Making apps",
-          done:false
-        }
-      ]
-    }
+        // {
+        //   title: "Go to shopping",
+        //   done: false,
+        // },
+        // {
+        //   title: "Go to gym",
+        //   done: false,
+        // },
+        // {
+        //   title: "Study",
+        //   done: false,
+        // },
+        // {
+        //   title: "Making apps",
+        //   done: false,
+        // },
+      ],
+    };
   },
   methods: {
-    deleteTask(index) { 
-      this.$q.dialog({
-        title: 'Confirm',
-        message: 'Do you wanna delete this task?',
-        cancel: true,
-        persistent: true
-      }).onOk(() => {
-        this.tasks.splice(index,1)
-        this.$q.notify({
-          message: "Task deleted!"
+    deleteTask(index) {
+      this.$q
+        .dialog({
+          title: "Confirm",
+          message: "Do you wanna delete this task?",
+          cancel: true,
+          persistent: true,
         })
-      })
+        .onOk(() => {
+          this.tasks.splice(index, 1);
+          this.$q.notify({
+            message: "Task deleted!",
+          });
+        });
     },
-    addTask(){
+    addTask() {
       this.tasks.push({
         title: this.newTask,
-        done: false
-      })
-      this.newTask=""
+        done: false,
+      });
+      this.newTask = "";
     },
-    editTask(){
-    console.log('Clicked');
-    }
-  }
-  
-}
-)
+    editTask() {
+      console.log("Clicked");
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-  .done{
-    .q-item__label{
-      text-decoration: line-through;
-      color: #b1b1b1;
-    }
+.done {
+  .q-item__label {
+    text-decoration: line-through;
+    color: #b1b1b1;
   }
-
-  .q-btn .q-icon{
-    font-size: 20px;
-  }
-  .q-item__section--main ~ .q-item__section--side {
-    padding-left: 0;
-    padding-right: 5px;
 }
 
-
- 
+.q-btn .q-icon {
+  font-size: 20px;
+}
+.q-item__section--main ~ .q-item__section--side {
+  padding-left: 0;
+  padding-right: 5px;
+}
+.no-task {
+  color: $primary;
+  opacity: 0.7;
+}
 </style>
